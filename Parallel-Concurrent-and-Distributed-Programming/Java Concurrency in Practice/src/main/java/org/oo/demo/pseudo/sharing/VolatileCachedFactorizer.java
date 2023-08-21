d394d0b07e8fc82d52e060d479fdf995c0f8cd71
OddUlrich <vmoonodd@gmail.com>
2023-08-22 00:13:52 +0800

    Immutable object to implement servlet service.

---
+++ Parallel-Concurrent-and-Distributed-Programming/Java Concurrency in Practice/src/main/java/org/oo/demo/pseudo/sharing/VolatileCachedFactorizer.java
@@ -0,0 +1,23 @@
+package org.oo.demo.pseudo.sharing;
+
+import org.oo.demo.iface.Servlet;
+import org.oo.demo.model.ServletRequest;
+import org.oo.demo.model.ServletResponse;
+
+import java.math.BigInteger;
+
+import static org.oo.demo.pseudo.threadsafety.CommonServletService.*;
+
+public class VolatileCachedFactorizer implements Servlet {
+    private volatile OneValueCache cache = new OneValueCache(null, null);
+
+    public void service(ServletRequest req, ServletResponse resp) {
+        BigInteger i = extractFromRequest(req);
+        BigInteger[] factors = cache.getFactors(i);
+        if (factors == null) {
+            factors = factor(i);
+            cache = new OneValueCache(i, factors);
+        }
+        encodeIntoResponse(resp, factors);
+    }
+}
