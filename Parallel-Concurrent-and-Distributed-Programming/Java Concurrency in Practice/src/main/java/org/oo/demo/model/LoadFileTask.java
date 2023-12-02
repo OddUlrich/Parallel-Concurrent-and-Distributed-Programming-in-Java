f906c5d952e5149b03d4fcc0c64fc8edc7345774
OddUlrich <vmoonodd@gmail.com>
2023-12-02 22:50:38 +0800

    Dead lock happens in a single executor.

---
+++ Parallel-Concurrent-and-Distributed-Programming/Java Concurrency in Practice/src/main/java/org/oo/demo/model/LoadFileTask.java
@@ -0,0 +1,15 @@
+package org.oo.demo.model;
+
+import java.util.concurrent.Callable;
+
+public class LoadFileTask implements Callable<String> {
+
+
+    public LoadFileTask(String fileName) {
+    }
+
+    @Override
+    public String call() throws Exception {
+        return null;
+    }
+}
---
+++ Parallel-Concurrent-and-Distributed-Programming/Java Concurrency in Practice/src/main/java/org/oo/demo/pseudo/threadpool/ThreadDeadlock.java
@@ -0,0 +1,29 @@
+package org.oo.demo.pseudo.threadpool;
+
+import org.oo.demo.model.LoadFileTask;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class ThreadDeadlock {
+
+    ExecutorService exec = Executors.newSingleThreadExecutor();
+
+    public class RenderPageTask implements Callable<String> {
+        public String call() throws Exception {
+            Future<String> header, footer;
+            header = exec.submit(new LoadFileTask("header.html"));
+            footer = exec.submit(new LoadFileTask("footer.html"));
+            String page = renderBody();
+
+            return header.get() + page + footer.get();
+        }
+    }
+
+    private String renderBody() {
+        return "";
+    }
+
+}
