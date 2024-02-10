b6f0809ad4b6e7ee46107c7a10f9d5be4596013a
OddUlrich <vmoonodd@gmail.com>
2024-02-10 18:38:23 +0800

    Atomicity implementation on CAS.

---
+++ Parallel-Concurrent-and-Distributed-Programming/Java Concurrency in Practice/src/main/java/org/oo/demo/pseudo/atomic/CasCounter.java
@@ -0,0 +1,22 @@
+package org.oo.demo.pseudo.atomic;
+
+import org.oo.demo.annotation.ThreadSafe;
+
+@ThreadSafe
+public class CasCounter {
+
+    private SimulatedCAS value;
+
+    public int getValue() {
+        return value.get();
+    }
+
+    public int increment() {
+        int v;
+        do {
+            v = value.get();
+        } while (v != value.compareAndSwap(v, v + 1));
+        return v + 1;
+    }
+
+}
